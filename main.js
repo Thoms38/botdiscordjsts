a950eeb3643d4ce71ec0782d40ccbcbf9b16aaf1
Thoms38 <thomas.saladini@gmail.com>
2019-09-07 10:54:27 +0200

    changement du bot

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Je suis prêt !");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("PONG !");
-  if (msg.content === "everyone")
-    msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone (noteveryone), salut à tous !");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
 });
 
-client.login("NjE5NTgzNTE3NzQ3MTgzNjM2.XXKWcQ.zKM-5heZcuRZNqQUKDj8d1drl9E");
+client.login(TOKEN);
